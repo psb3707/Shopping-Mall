aa366a99b5a0f6d3d0766ec90803957e93315519
psb <psb3707@naver.com>
2024-07-04 16:13:09 +0900

    third commit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h^rswk5*_kwk5@+e0fzl!!iaq)sueo9j_8i^37rpqyzi$b-@sm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.39.213.133','127.0.0.1']
 
 
 # Application definition
